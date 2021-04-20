00879171c5630bce7b9812f57b32be55ecffbf75
Alberic <alberic.ziangbe@ism.edu.sn>
2021-04-20 13:44:32 +0000

    feature: Update movement function

--- elements.py
+++ elements.py
@@ -71,14 +71,6 @@ def getTrajectory(sets):
     return sets.get("trajectory")
 
 
-'''
-def getAllTrajectories(setsLists):
-    allTrajectories = []
-    for i in setsLists:
-        allTrajectories.append(getTrajectory(i))
-    return allTrajectories
-'''
-
 
 def overlaps(firstSet, secondSet):
     "Determine si deux pions (losanges) se chevauchent ou pas"
@@ -92,85 +84,32 @@ def overlaps(firstSet, secondSet):
     return overlaping
 
 
-'''
-def move(sets):
-    #sets = ensemble
-    x, y = sets.get('rhomb')[0], sets.get('rhomb')[1]
-    trajectory = getTrajectory(sets)
-    dx, dy = 0, 0
-    for i in range(len(trajectory)-1):
-        #print(trajectory[i:i+2])
-        if trajectory[i][0] / trajectory[i+1][0] == 1:
-            #Mvt suivant l'axe des ordonnees
-            if y <= trajectory[i+1][1]:
-                y, dx, dy = y+(RADUIS*2), dx, RADUIS
-        if trajectory[i][1] / trajectory[i+1][1] == 1:
-            #Mvt suivant l'axe des abcisses
-            if x <= trajectory[i+1][0]:
-                x, dx, dy = x+(RADUIS*2), RADUIS, dy
-        if trajectory[i][0] / trajectory[i+1][0] != 1 and trajectory[i][1] / trajectory[i+1][1] != 1:
-            #Mvt oblique
-            x, y, dx, dy = x+(RADUIS*2), y+(RADUIS*2), trajectory[i][0] / trajectory[i+1][0], trajectory[i][1] / trajectory[i+1][1]
-        sets.get('rhomb')[0], sets.get('rhomb')[1] = x, y
-        
-        can.coords(sets.get('rhomb_id')[0],sets.get('rhomb')[0],sets.get('rhomb')[1],sets.get('rhomb')[0]+RADUIS,sets.get('rhomb')[1]+RADUIS)
-        can.coords(sets.get('rhomb_id')[1],sets.get('rhomb')[0],sets.get('rhomb')[1],sets.get('rhomb')[0]+RADUIS,sets.get('rhomb')[1]+RADUIS)
-    fen.after(20, move(sets))
-
 def move_rhomb(event):
     global setsLists
-    for sets in setsLists:
-        move(sets)
-    
-'''
-'''
-def move():
-    global sets
-    x, y = sets.get('rhomb')[0], sets.get('rhomb')[1]
-    trajectory = getTrajectory(sets)
-    dx, dy = 0, 0
-    i = 0
-    if trajectory[i][0] / trajectory[i+1][0] == 1:
-        #Mvt suivant l'axe des ordonnees
-        if y <= trajectory[i+1][1]:
-            y, dx, dy = y+(RADUIS*2), dx, RADUIS
-    if trajectory[i][1] / trajectory[i+1][1] == 1:
-        #Mvt suivant l'axe des abcisses
-        if x <= trajectory[i+1][0]:
-            x, dx, dy = x+(RADUIS*2), RADUIS, dy
-    if trajectory[i][0] / trajectory[i+1][0] != 1 and trajectory[i][1] / trajectory[i+1][1] != 1:
-        #Mvt oblique
-        x, y, dx, dy = x+(RADUIS*2), y+(RADUIS*2), trajectory[i][0] / trajectory[i+1][0], trajectory[i][1] / trajectory[i+1][1]
-    sets.get('rhomb')[0], sets.get('rhomb')[1] = x, y
-    can.coords(sets.get('rhomb_id')[0],sets.get('rhomb')[0],sets.get('rhomb')[1],sets.get('rhomb')[0]+RADUIS,sets.get('rhomb')[1]+RADUIS)
-    can.coords(sets.get('rhomb_id')[1],sets.get('rhomb')[0],sets.get('rhomb')[1],sets.get('rhomb')[0]+RADUIS,sets.get('rhomb')[1]+RADUIS)
-    i += 1
-    fen.after(20, move)
-'''
+    print(event)
+    move(setsLists[0])
+    print(setsLists[0].get('rhomb'))
 
 
 def move(sets):
-    #sets = ensemble
     global dx, dy
-    x, y = sets.get('rhomb')[0], sets.get('rhomb')[
-        1]  # === les coordonés x,y du losange
+    #x, y = sets.get('rhomb')[0], sets.get('rhomb')[1]  # === les coordonés x,y du losange
     trajectory = getTrajectory(sets)  # ====liste des trajectoires
     for i in range(len(trajectory)-1):  # on parcour les trajectoires possible
-        #print(trajectory[i], trajectory[i+1])
-        # break
+        print('Boucle ', i, trajectory[i], trajectory[i+1])
         dx,dy=0,0
-        print('Boucle', i)
+        x, y = trajectory[i][0], trajectory[i][1]
         mouvement(trajectory[i], trajectory[i+1], x, y)
-
+        
 
 def mouvement(a, b, x, y):
     global dx, dy
     # on vérifie si xi=xi+1
+    print('mouvement')
     
     if a[0] / b[0] == 1:
         # Mvt suivant l'axe des ordonnees, vertical
         if y <= b[1]:
-            #y, dx, dy = y+(RADUIS*2), dx, RADUIS
             dx = 0
             dy = 1
         else:
@@ -180,7 +119,6 @@ def mouvement(a, b, x, y):
     if a[1] / b[1] == 1:
         # Mvt suivant l'axe des abcisses, horizontal
         if x <= b[0]:
-            #x, dx, dy = x+(RADUIS*2), RADUIS, dy
             dx = 1
             dy = 0
         else:
@@ -188,33 +126,15 @@ def mouvement(a, b, x, y):
             dy = 0
     x = x+(dx*10)
     y = y+(dy*10)
-    # affecter la ligne de trajectoire
-
-    #x, y, dx, dy = x+(RADUIS*2), y+(RADUIS*2), trajectory[i][0] / trajectory[i+1][0], trajectory[i][1] / trajectory[i+1][1]
-    # ne pas affecter le dictionnaire principale???
-    sets.get('rhomb')[0], sets.get('rhomb')[1] = x, y
     can.coords(sets.get('rhomb_id')[0],
                x-RADUIS, y,
                x, y-RADUIS,
                x+RADUIS, y,
                x, y+RADUIS
                )
-    # can.coords(sets.get('rhomb_id')[0],
-    #             x-(RADUIS-15), y,
-    #             x, y-(RADUIS-15),
-    #             x+(RADUIS-15), y,
-    #             x, y+(RADUIS-15)
-    #           )   
-
-    if x != b[0] and y != b[1]:
-        #print(x, y, a, b)
-        print('Recursivite')
+    print("x, y = ", x,y)
+    if x != b[0] or y != b[1]:
         fen.after(10, mouvement(a, b, x, y))
-
-
-def move_rhomb(event):
-    global setsLists
-    move(setsLists[0])
 
 
 # --------------------------Consts--------------------------
@@ -227,7 +147,7 @@ can.tag_click = True
 can.pack(padx=5, pady=5)
 
 
-# TEST
+#TEST
 #allTrajectories = getAllTrajectories(setsLists)
 #print(len(allTrajectories), allTrajectories)
 
@@ -237,8 +157,7 @@ can.pack(padx=5, pady=5)
     ))
 '''
 setsLists = [
-    elementsDictionnary([400, 100], [[400, 100], [400, 400], [700, 400], [
-                        700, 500]], [700, 500], create_rhomb(can, 400, 100, RADUIS))
+    elementsDictionnary([400, 100], [[400, 100], [400, 400], [700, 400], [700, 500]], [700, 500], create_rhomb(can, 400, 100, RADUIS))
 ]
 for sets in setsLists:
     create_trajectory(can, sets.get('trajectory'))
