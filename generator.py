f2be072c98061acd806fb6ae907013fdb2d07f59
Diani-Mamoudou <63793590+Diani-Mamoudou@users.noreply.github.com>
2021-05-15 01:51:26 +0000

    commit generator

--- generator.py
+++ generator.py
@@ -1,4 +1,4 @@
-import tkinter, elements, app, random, consts
+import tkinter, elements, random, consts
 
 def number_pawn(listeGene):
     return len(listeGene)+1
@@ -6,62 +6,79 @@ def number_pawn(listeGene):
 
 def pawn_position():
     positionPion=[]
-    positionPion.append(random.randint(200, 800))
-    positionPion.append(random.randint(200, 600))
+    positionPion.append(10*random.randint(20, 80))
+    positionPion.append(10*random.randint(20, 60))
     return positionPion
 
-def number_break():
-    return random.randint(0,4)
+def number_break(pI, pF):
+    if pI[0]==pF[0] or pI[1]==pF[1]:
+        return random.randint(0,3)
+    else:
+        return random.randint(1,3)
 
 def one_break(pI, pF):
     choix = [1, 2]
-    L=[]
     c=random.choice(choix)
     if c==1: 
-        L=[pI[0], pF[1]]
-    else:
         L=[pF[0], pI[1]]
-    return L
+    else:
+        L=[pI[0], pF[1]]
+    return [L]
     
 def two_break(pI, pF):
     choix = [1, 2]
     L=[]
     c=random.choice(choix)
     if c==1:
-        L.append(random.randint(200, 800))
-        L.append(pF[1])
+        L.append(10*random.randint(20, 80))
+        L.append(pI[1])
+        L1=[L[0], pF[1]]
     else:
-        L.append(pF[0])
-        L.append(random.randint(200, 600))
-    L1=one_break(pI, L)
-    trajectory=[L1,L]
+        L.append(pI[0])
+        L.append(10*random.randint(20, 60))
+        L1=[pF[0], L[1]]
+    
+    #L1=one_break(L, pF)
+    trajectory=[L,L1]
     return trajectory
 
 def three_break(pI, pF):
     choix = [1, 2]
     L=[]
+    L1=[]
     c=random.choice(choix)
     if c==1:
-        L.append(random.randint(200, 800))
-        L.append(pF[1])
+        L.append(10*random.randint(20, 80))
+        L.append(pI[1])
+
+        L1=[L[0]]
+        L1.append(10*random.randint(20, 60))
+
+        L2=[pF[0],L1[1]]
     else:
-        L.append(pF[0])
-        L.append(random.randint(200, 600))
-    L1=two_break(pI, L)
-    trajectory=L1.append(L)
+        L.append(pI[0])
+        L.append(10*random.randint(20, 60))
+
+        L1.append(10*random.randint(20, 80))
+        L1.append(L[1])
+
+        L2=[L1[0],pF[1]]
+
+    trajectory=[L,L1,L2]
     return trajectory
 
 
 
 def create_trajectory(pI, pF):
-    nb_courbure=number_break()
+    nb_courbure=number_break(pI, pF)
+    print(nb_courbure)
     trajectory=[pI]
     if nb_courbure==1:
-        trajectory=+one_break(pI, pF)
+        trajectory+=one_break(pI, pF)
     elif nb_courbure==2:
-        trajectory=+two_break(pI, pF)
+        trajectory+=two_break(pI, pF)
     elif nb_courbure==3:
-        trajectory=+three_break(pI, pF)
+        trajectory+=three_break(pI, pF)
     trajectory.append(pF)
     return trajectory
 
@@ -72,6 +89,7 @@ def generator(setsLists):
         positionPion=pawn_position()
         positionEncoche=pawn_position()
         trajectoire=create_trajectory(positionPion, positionEncoche)
+        print (positionPion,positionEncoche,trajectoire)
         eleDico= elements.elementsDictionnary(positionPion, trajectoire, positionEncoche, elements.create_rhomb(consts.can, positionPion[0], positionPion[1], consts.RADUIS))
         listPart.append(eleDico)
     return listPart
